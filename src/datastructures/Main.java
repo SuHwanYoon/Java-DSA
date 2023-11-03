34a069f9cd53669773b9fe3a4c6f986c0b6c4880
SuHwanYoon <ehtldy@naver.com>
2023-11-03 11:38:00 +0900

    add Hello DSA

--- src/datastructures/Main.java
+++ src/datastructures/Main.java
@@ -2,6 +2,6 @@ package datastructures;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello DSA!");
+        System.out.println("Hello DSA Practice!");
     }
 }
